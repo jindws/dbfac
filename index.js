2c7884a355404654ec93171de8014e6eae44a951
金大伟 <dawei.jin@xiaobao100.com>
2018-10-23 20:30:44 +0800

    1.3.6

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const os = require('object-param')
+const os = require('object-param').default
 require('whatwg-fetch')
 
 function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
